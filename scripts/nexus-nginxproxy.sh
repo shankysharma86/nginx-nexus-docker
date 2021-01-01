bb6696519eb25fdbcb227a6a33c05b5b61300296
Shashank.Sharma@cognizant.com-at-433593965157 <>
2021-01-01 11:58:34 -0600

    Updated scripts

--- scripts/nexus-nginxproxy.sh
+++ scripts/nexus-nginxproxy.sh
@@ -1,12 +1,27 @@
+#!/bin/bash
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # ............................... DISCLAIMER ............................ #
 #
-#These scripts come without warranty of any kind. Use them at your own risk. 
+#These scripts come without warranty of any kind. Use them at your own risk.
 #I assume no liability for the accuracy, correctness, completeness, or usefulness
-#of any information provided by this script nor for any sort of damages using 
+#of any information provided by this script nor for any sort of damages using
 #these scripts may cause.
 # ....................................................................... #
 
-#!/bin/bash
+# Create certs directory
+mkdir ../certs
 
 # Generate Root Key rootCA.key with 2048
 openssl genrsa -passout pass:"$1" -des3 -out ../certs/rootCA.key 2048
@@ -33,10 +48,7 @@ fi
 
 # Generate nexus Cert
 openssl req -subj "/C=US/ST=Random/L=Random/O=Global Security/OU=IT Department/CN=localhost"  -new -sha256 -nodes -out ../certs/nexus.csr -newkey rsa:2048 -keyout ../certs/nexuskey.pem
-openssl x509 -req -passin pass:"$1" -in ../certs/nexus.csr -CA ../certs/rootCA.pem -CAkey ../certs/rootCA.key -CAcreateserial -out ../certs/nexuscert.crt -days 500 -sha256 -extfile  <(printf "subjectAltName=DNS:localhost,DNS:nexus-repo")
-
-# Create certs directory
-mkdir ../certs
+openssl x509 -req -passin pass:"$1" -in ../certs/nexus.csr -CA ../certs/rootCA.pem -CAkey ../certs/rootCA.key -CAcreateserial -out ../certs/nexuscert.crt -days 500 -sha256 -extfile <(printf "subjectAltName=DNS:localhost,DNS:nexus-repo")
 
 cd ../nginx/
 echo $PWD
